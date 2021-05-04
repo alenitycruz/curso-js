6b422b1d68a338974d8fb84f019847bda4e0e207
Alessandra Cruz <alessandradsmcruz@recife.pe.senac.br>
2021-05-04 14:47:04 -0300

    condicionais js
    mais comandos: isolados e em site

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,1 +1,6 @@
-console.log('O console funcionou corretamente');
+var vel = 78.2
+console.log(`A velocidade do seu carro é ${vel} km/h `);
+if (vel > 60) { //condição simples!
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija usando sempre o cinto de segurança`);
