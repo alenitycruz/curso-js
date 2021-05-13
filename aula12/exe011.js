1aac95b9455dad4c5b1355cb15e9ca6c9369c4d4
Alessandra Cruz <alessandradsmcruz@recife.pe.senac.br>
2021-05-13 16:54:00 -0300

    mais exercicios condicionais

--- aula12/exe011.js
+++ aula12/exe011.js
@@ -1,8 +1,11 @@
 var idade = 22
+console.log(`Você tem ${idade} anos.`)
 if (idade<16) {
     console.log('Não vota')
 }else {
     if(idade => 16 && idade < 18){
         console.log('Voto opcional')
+    }else {
+        console.log('Voto obrigatório')
     }
 }
