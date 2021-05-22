b13ee50368b07dec3760487556a849f47abe4838
Alessandra Cruz <alessandradsmcruz@recife.pe.senac.br>
2021-05-22 00:14:14 -0300

    alteraçoes

--- aula12ex/modelo/script.js
+++ aula12ex/modelo/script.js
@@ -1,1 +1,17 @@
-alert("Olá!")
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var foto = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //Bom dia!
+        img.src = manha.jpg
+    }else if (hora >=12 && hora < 18) {
+        //Boa tarde!
+        img.src = tarde.jpg
+    }else {
+        //Boa Noite!!
+        img.src = noite.jpg
+    }
+}
