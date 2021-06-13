080ad907bd8cbf2e4b4a19d87b5833c72b5b6532
Alessandra Cruz <alessandradsmcruz@recife.pe.senac.br>
2021-06-13 16:44:42 -0300

    novos exercicios

--- aula12ex/exe014/script.js
+++ aula12ex/exe014/script.js
@@ -1,1 +1,27 @@
-alert("Olá!")
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if(fano.value.length == 0 || fano.value > ano) {
+        window.alert('[erro] Verifique e tente novamente')
+    } else {
+        var fsex = document.getElementById('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id','foto')
+        if(fsex[0].checked) {
+            genero = 'Homem'
+            if(idade >=0 && idade < 10) {
+                //Criança
+            }else if (idade < 21) {
+                //Jovem
+            }
+        } else {
+            genero = 'Mulher'
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+    }
+}
