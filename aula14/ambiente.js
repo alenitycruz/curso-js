f36cc0939df07b62a87c7c2656f816593627b502
Alessandra Cruz <alessandradsmcruz@recife.pe.senac.br>
2021-08-03 22:33:55 -0300

    Create ambiente.js

---
+++ aula14/ambiente.js
@@ -0,0 +1,17 @@
+for(var c = 1 ;c <= 5; c++){
+    console.log(`Passo ${c}`)
+}
+
+
+/*var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+}while (c <= 5)*/
+
+
+/*var c = 1
+while (c <= 5) {
+    console.log(`Passo ${c}`)
+    c++
+}*/
